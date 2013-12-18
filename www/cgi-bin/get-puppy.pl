69196e1a9b1ac1c78d288c9b42e26f5ace21f175
Martin Baulig <martin.baulig@xamarin.com>
2013-12-18 08:21:00 +0000

    Add the remote port.

--- www/cgi-bin/get-puppy.pl
+++ www/cgi-bin/get-puppy.pl
@@ -12,8 +12,10 @@ my $method = request_method();
 my $path_info = path_info();
 my $auth = auth_type();
 my $remote = remote_addr();
+my $port = $ENV{"REMOTE_PORT"};
 
 print "METHOD: $method\n";
 print "PATH: $path_info\n" if defined $path_info;
 print "AUTH: $auth\n" if defined $auth;
 print "REMOTE: $remote\n";
+print "PORT: $port\n";
