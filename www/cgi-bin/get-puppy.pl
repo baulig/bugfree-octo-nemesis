88edf8f3097ace61583f15880dca150aa1bc866d
Martin Baulig <martin.baulig@xamarin.com>
2013-12-19 20:10:56 +0000

    get-puppy.pl now supports a "?mode=<mode>" parameter.
    Mode is one of:
    * default: Same as not specifying the parameter, does not include
      any Content-Length: header.
    * length: Include Content-Length: header.
    * chunked: Use chunked transfer encoding.

--- www/cgi-bin/get-puppy.pl
+++ www/cgi-bin/get-puppy.pl
@@ -2,11 +2,7 @@
 use strict;
 use CGI qw/:cgi/;
 
-# my $q = CGI->new();
-# print $q->header('text/plain');
-# my %params = $q->Vars;
-
-print "Content-type: text/plain\n\n";
+my $q = CGI->new();
 
 my $method = request_method();
 my $path_info = path_info();
@@ -14,8 +10,34 @@ my $auth = auth_type();
 my $remote = remote_addr();
 my $port = $ENV{"REMOTE_PORT"};
 
-print "METHOD: $method\n";
-print "PATH: $path_info\n" if defined $path_info;
-print "AUTH: $auth\n" if defined $auth;
-print "REMOTE: $remote\n";
-print "PORT: $port\n";
+my $output = "";
+open OUTPUT, '>', \$output;
+
+print OUTPUT "METHOD: $method\n";
+print OUTPUT "PATH: $path_info\n" if defined $path_info;
+print OUTPUT "AUTH: $auth\n" if defined $auth;
+print OUTPUT "REMOTE: $remote\n";
+print OUTPUT "PORT: $port\n";
+
+close OUTPUT;
+
+print "Content-Type: text/plain\n";
+
+my $mode = $q->param("mode");
+$mode = "default" unless defined $mode;
+
+if ($mode eq "chunked") {
+    print "Transfer-Encoding: chunked\n\n";
+    my $length = length($output);
+    printf "%x\r\n", $length;
+    print "$output\r\n";
+    print "0\r\n\r\n";
+} elsif ($mode eq "length") {
+    my $length = length($output);
+    print "Content-Length: $length\n\n";
+    print $output;
+} else {
+    print "\n";
+    print $output;
+}
+
